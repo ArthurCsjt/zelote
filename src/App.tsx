e2b74330dc93a6ceb28d53fcf63d9b5454fe71c5
ArthurCsjt <arthur.alencar@colegiosaojudas.com.br>
2025-10-06 07:00:35 -0300

    Erro de database

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { MobileToaster } from "@/components/ui/mobile-toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip"; // ALTERAÇÃO 2: Desativado para o teste
+// import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// ALTERAÇÃO 1: Corrigimos as importações para a nova arquitetura
 import { AuthProvider } from "./providers/AuthProvider";
 import { useAuth } from "./contexts/AuthContext";
+
+// PASSO 1: IMPORTAR O PROVIDER DO BANCO DE DADOS
+import { DatabaseProvider } from './contexts/DatabaseContext'; // Ajuste o caminho se necessário
 
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import Index from "./pages/Index";
@@ -28,30 +30,33 @@ const App = () => (
   <ErrorBoundary>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        {/* <TooltipProvider> */} {/* ALTERAÇÃO 2: Desativado para o teste */}
-          <MobileToaster />
-          <Sonner />
-          
-          <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              
-              <Route path="/" element={
-                <ProtectedRoute>
-                  <Index />
-                </ProtectedRoute>
-              } />
-              
-              <Route path="/settings" element={
-                <ProtectedRoute>
-                  <Settings />
-                </ProtectedRoute>
-              } />
-              
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        {/* </TooltipProvider> */} {/* ALTERAÇÃO 2: Desativado para o teste */}
+        {/* PASSO 2: O DATABASEPROVIDER DEVE ENVOLVER TODO O RESTO DA APLICAÇÃO */}
+        <DatabaseProvider>
+          {/* <TooltipProvider> */}
+            <MobileToaster />
+            <Sonner />
+            
+            <BrowserRouter>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                
+                <Route path="/" element={
+                  <ProtectedRoute>
+                    <Index />
+                  </ProtectedRoute>
+                } />
+                
+                <Route path="/settings" element={
+                  <ProtectedRoute>
+                    <Settings />
+                  </ProtectedRoute>
+                } />
+                
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          {/* </TooltipProvider> */}
+        </DatabaseProvider>
       </AuthProvider>
     </QueryClientProvider>
   </ErrorBoundary>
