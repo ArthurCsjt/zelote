6a83ec35e4eee1981d57161b0f9590f97a6d4ae7
ArthurCsjt <arthur.alencar@colegiosaojudas.com.br>
2026-02-05 11:19:00 -0300

    erro BellOff is not defined aconteceu porque eu usei os ícones novos mas esqueci de importá-los pacote lucide-react.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { User, LogOut, ArrowLeft, Bell, Settings, Sun, Moon, Loader2, Activity } from 'lucide-react';
+import { User, LogOut, ArrowLeft, Bell, Settings, Sun, Moon, Loader2, Activity, BellRing, BellOff } from 'lucide-react';
 import { useAuth } from '@/contexts/AuthContext';
 import { useTheme } from '@/hooks/use-theme';
 import { useNavigate } from 'react-router-dom';
